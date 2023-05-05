704054a70b556414eaadc77ae1fed822829d71b6
Mary <99505177+meggo15@users.noreply.github.com>
2023-05-05 13:57:31 -0700

    Updated About page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -19,19 +19,32 @@ get_header(); ?>
 	<article class="about-content">
 		<h1>Accelerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h1>
 		<h2>OUR SERVICES</h2>
-		
-     <?php while ( have_posts() ) : the_post(); ?>
-	 		<h5><?php echo $title; ?></h5>
-			<h6><?php the_content(); ?></h6>
-			<div class="about-services-images">
-		<?php if($graphic) {
-			echo wp_get_attachment_image( $graphic, $size);
-		} ?>
-		</div>
-
-		<?php endwhile; // end of the loop. ?>
-    
+		<p>We take pride in our clients and the content we create for them. Here is a brief overview of our offered services.</p>
+		<?php query_posts ('post_type=about_services'); ?>
+    	<?php while ( have_posts() ) : the_post();	
+			$title = get_field('service_title');
+			$graphic = get_field('graphic');
+			$size = "thumbnail"; ?>
+			<article class="about-services">
+				<aside class="about-services-sidebar">
+					<h5><?php echo $title; ?></h5>
+					<h6><?php the_content(); ?></h6>
+				</aside>
+				<div class="about-services-images">
+				<?php if($graphic) {
+					echo wp_get_attachment_image( $graphic, $size);
+				} ?>
+				</div>
+			</article>
+			
+		<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
 	</article>
+	<aside>
+				<h2>Interested in Working with Us?</h2>
+				<a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+	</aside>
+
 			
 	</div><!-- .main-content -->
 </div><!-- #primary -->
